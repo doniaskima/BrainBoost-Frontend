e9a714407247bfef22903e99400021522d4bb9d5
unknown <doniaskima344@gmail.com>
2023-05-31 17:16:37 +0100

    Add group thunks and update store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,12 @@
 import { ThunkDispatch } from 'redux-thunk';
 import { UserAction } from './actions/user';
 import { MessageActionTypes } from './actions/message';
+import { GroupActionTypes } from './actions/group';
 import configureStore from './configureStore';
 
 const store = configureStore();
 
 export type AppState = ReturnType<typeof store.getState>;
-export type thunkDispatch = ThunkDispatch<AppState, any, UserAction<any> | MessageActionTypes>;
+export type thunkDispatch = ThunkDispatch<AppState, any, UserAction<any> | MessageActionTypes | GroupActionTypes>;
 
 export { store };
