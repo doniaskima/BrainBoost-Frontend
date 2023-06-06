573e85bd03b0f33ec88437d489195fa863260e6c
unknown <doniaskima344@gmail.com>
2023-06-06 22:33:57 +0100

    layout

--- src/pages/StudyWithme.tsx
+++ src/pages/StudyWithme.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Cog6ToothIcon } from "@heroicons/react/24/solid";
+import { IconButton } from "@material-tailwind/react";
+import {
+  Sidenav,
+  DashboardNavbar,
+  Configurator,
+ 
+} from "../layout/index";
+import routes from "../routes";
+import { useMaterialTailwindController } from "../context/index";
+import { useState } from "react";
+ 
 
-const StudyWithme = () => {
-  return <div>StudyWithme</div>;
-};
+export function StudyWithme() {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { sidenavType } = controller;
+  const [openConfigurator,setOpenConfigurator]=useState(false)
 
+  return (
+    <div className="min-h-screen bg-blue-gray-50/50">
+      <Sidenav
+        routes={routes}
+        brandImg={
+          sidenavType === "dark" ? "/img/BrainBoost.png" : "/img/BrainBoost.png"
+        }
+      />
+      <div className="p-4 xl:ml-80">
+        <DashboardNavbar />
+        <Configurator />
+        <IconButton
+          size="lg"
+          color="white"
+          className="fixed bottom-8 right-8 z-40 rounded-full shadow-blue-gray-900/10"
+          ripple={false}
+          onClick={() => setOpenConfigurator(!openConfigurator)}
+        >
+          <Cog6ToothIcon className="h-5 w-5" />
+        </IconButton>
+        <Routes>
+          {routes.map(
+            ({  pages }) =>
+              pages.map(({ path, element }) => (
+                <Route  path={path} element={element} />
+              ))
+          )}
+        </Routes>
+    
+      </div>
+    </div>
+  );
+}
+
+ 
 export default StudyWithme;
