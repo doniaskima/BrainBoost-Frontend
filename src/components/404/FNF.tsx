6b4a5014e0f698532e0fc651fe8419f12bfe23cb
unknown <doniaskima344@gmail.com>
2023-06-06 16:42:47 +0100

    404

--- src/components/404/FNF.tsx
+++ src/components/404/FNF.tsx
@@ -1,3 +1,18 @@
 import React from 'react';
 import error from "../../assets/download.png"
 
+const FNf: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen bg-white flex-col lg:col-span-2 lg:w-auto">
+        <img src={error} alt="w-56 h-56 items-center m-4"   />
+        <h4 className="text-gray-900">
+         This page is not available.
+        </h4>
+        <p style={{ textAlign: 'center' }}>
+        The URL is broken or must be removed from our server. That's all we know.
+        </p>
+    </div>
+  );
+};
+
+export default FNf;
