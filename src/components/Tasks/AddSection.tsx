27b881c190861a03608b53b9dfd475e32660d034
unknown <doniaskima344@gmail.com>
2023-06-17 01:15:59 +0100

    Add Section Model

--- src/components/Tasks/AddSection.tsx
+++ src/components/Tasks/AddSection.tsx
@@ -1,40 +1,43 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState } from 'react';
+import { Modal } from 'react-bootstrap';
 import { toast } from 'react-toastify';
 import { Button } from 'reactstrap';
-import { Section } from './InterfaceTask';
-import { Dropdown, Modal } from 'react-bootstrap';
-
+ 
+ 
 interface Props {
-  projectId: string;
-  size: any;
   showModal: { status: boolean; setStatus: (value) => void };
   dataTasks: { data: Array<Section>; setData: (data) => void };
+  projectId: string;
+  size: any;
 }
-
 const AddSection: React.FC<Props> = (props: Props) => {
   const [showModal, setShowModal] = useState(false);
-  console.log(showModal)
   const [nameSection, setNameSection] = useState('');
-
+  const [descriptionSection, setDescriptionSection] = useState('');
+  const [err, setErr] = useState(null);
+  const addSection = () => {
+    
+  };
   return (
     <div className="add-section">
       <div
         style={{ height: '100%' }}
         onClick={() => {
           props.showModal.setStatus(false);
-        }}
-      >
+        }}>
         <div className="column-tasks">
           <div className="column-task-sort">
-            <Button
-              color="primary"
-              onClick={() => {
-                setShowModal(true);
-              }}
-            >
-              <i className="fa fa-list-alt mr-2 ml-2" aria-hidden="true"></i>
-              <span>Add Section</span>
-            </Button>
+            <div className="board-task">
+              <Button
+                color="primary"
+                onClick={() => {
+                  setShowModal(true);
+                }}>
+                <i className="fa fa-list-alt" aria-hidden="true"></i>
+                <span> Add Section</span>
+              </Button>
+            </div>
           </div>
         </div>
       </div>
@@ -44,80 +47,54 @@ const AddSection: React.FC<Props> = (props: Props) => {
         className="modal-confirm"
         onHide={() => {
           props.showModal.setStatus(false);
-        }}
-      >
+        }}>
         <Modal.Header className="pb-0">
           <h1>Create New Section</h1>
         </Modal.Header>
         <Modal.Body>
           <form className="new-event--form">
             <div className="form-group">
-              <label className="form-control-name mr-2">Task name :</label>
               <input
                 type="text"
-                className='outline-none mt-2 mb-3'
                 placeholder="Enter a new section name"
+                className="form-control-alternative new-event--title form-control"
                 onChange={(e) => {
                   setNameSection(e.target.value);
                 }}
               />
             </div>
             <div className="form-group">
-              <label className="d-block mb-3 form-control">Section</label>
-              <div className="bd-highlight task-body-second">
-                <Dropdown
-                  onClick={(e) => {
-                    e.stopPropagation();
-                  }}
-                >
-                  <Dropdown.Toggle style={{ padding: '10px' }}>
-                     CurrentSection
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                         DropDown Item
-                     </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
+              <label className="form-control-label">Description</label>
+              <textarea
+                style={{ height: '100px' }}
+                placeholder="Desctiption"
+                className="form-control-alternative edit-event--description textarea-autosize form-control mr-2"
+                onChange={(e) => {
+                  setDescriptionSection(e.target.value);
+                }}></textarea>
+              <i className="form-group--bar"></i>
             </div>
-            <div className="form-group">
-                <label className='form-control-label d-block mb-3'>Label</label>
-                <div 
-                className="btn-group-toggle btn-group-colors event-tag btn-group align-items-center task-body-second"
-                role="group"
-                data-toggle="buttons"
-
-                >
-                </div>
-            </div>
+            <div className="d-flex justify-content-start align-items-center"></div>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <div>
-            <Button
-              style={{
-                backgroundColor: '#5e72e4',
-              }}
-              onClick={(e) => {
-                 
-              }}>
-              Create
-            </Button>
-          </div>
           <button
             type="button"
-            className="ml-auto btn btn-link"
-            onClick={() => props.showModal.setStatus(false)}
-            style={{
-              color: '#5e72e4',
+            className="btn btn-secondary"
+            onClick={() => {
+              setShowModal(false);
             }}>
             Close
+          </button>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={addSection}>
+             Create
           </button>
         </Modal.Footer>
       </Modal>
     </div>
   );
 };
-
 export default AddSection;
--- src/components/Tasks/Board2.tsx
+++ src/components/Tasks/Board2.tsx
@@ -1,102 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { useParams } from 'react-router';
-import { Section, Task ,Label } from './InterfaceTask';
 import { toast } from 'react-toastify';
+import { Button } from 'reactstrap';
+import { Dropdown, Modal } from 'react-bootstrap';
+import { Section, Task, Label } from './InterfaceTask';
 import AddSection from './AddSection';
 
 function Board2() {
-  
-   
   const [userId, setUserId] = useState('');
   const [taskDetails, setTaskDetails] = useState<Task>(null);
   const [showTaskDetails, setShowTaskDetails] = useState(false);
   const [dataTasks, setDataTasks] = useState<Array<Section>>([]);
   const [labels, setLabels] = useState<Array<Label>>([]);
-  const {projectId} = useParams();
-  console.log(projectId);
+  const { projectId } = useParams();
+
   const onDragEnd = (result) => {
-    // if (!result.destination) {
-    //   return;
-    // }
-    // let taskId = result.draggableId;
-    // let sectionFromId = result.source.droppableId;
-    // let indexFrom = result.source.index;
-    // let sectionToId = result.destination.droppableId;
-    // let indexTo = result.destination.index;
-    // if (sectionToId === sectionFromId) {
-    //   let section = dataTasks.filter(
-    //     (section) => section._id === sectionFromId,
-    //   )[0];
-    //   let task = section.tasks[indexFrom];
-    //   section.tasks.splice(indexFrom, 1);
-    //   let listTask1 = [...section.tasks];
-    //   let listTask2 = [...section.tasks];
-    //   section.tasks = [
-    //     ...listTask1.splice(0, indexTo),
-    //     task,
-    //     ...listTask2.splice(indexTo),
-    //   ];
-    //   setDataTasks(dataTasks);
-    // } else {
-    //   let sectionFrom = dataTasks.filter(
-    //     (section) => section._id === sectionFromId,
-    //   )[0];
-    //   let sectionTo = dataTasks.filter(
-    //     (section) => section._id === sectionToId,
-    //   )[0];
-    //   let taskFrom = sectionFrom.tasks[indexFrom];
-    //   sectionFrom.tasks.splice(indexFrom, 1);
-    //   let listTask1 = [...sectionTo.tasks];
-    //   let listTask2 = [...sectionTo.tasks];
-    //   sectionTo.tasks = [
-    //     ...listTask1.splice(0, indexTo),
-    //     taskFrom,
-    //     ...listTask2.splice(indexTo),
-    //   ];
-    // }
-    // taskService
-    //   .changeSection({
-    //     projectId: projectId,
-    //     taskId: taskId,
-    //     sectionId1: sectionFromId,
-    //     sectionId2: sectionToId,
-    //     index: indexTo,
-    //   })
-    //   .then((res) => {
-    //     setDataTasks(res.data.data.allTasks);
-    //   })
-    //   .catch((err) => {
-    //     toast.error(
-    //       err.response.data.error || "An unexpected error has occurred ",
-    //     );
-    //   });
+    // Drag and drop logic
   };
 
-
   return (
-   <div className="tasks"
-   onClick={() => {
-    setShowTaskDetails(false);
-   }}
-   >
+    <div className="tasks" onClick={() => setShowTaskDetails(true)}>
       <DragDropContext onDragEnd={onDragEnd}>
-          
+        Drag and Drop
       </DragDropContext>
-       <AddSection
+      <AddSection
         dataTasks={{
-        data:dataTasks,
-        setData:setDataTasks,
+          data: dataTasks,
+          setData: setDataTasks,
         }}
         showModal={{
-        status:showTaskDetails,
-        setStatus: setShowTaskDetails,
-       }}
+          status: showTaskDetails,
+          setStatus: setShowTaskDetails,
+        }}
         projectId={projectId}
         size={'xl'}
-       />
-   </div>
-  )
+      />
+    </div>
+  );
 }
 
-export default Board2
+export default Board2;
